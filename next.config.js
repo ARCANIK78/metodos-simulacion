5ac866d2c18f93d44d131a76e000b41b28818bff
Arcanik <loisgordisrour@gmail.com>
2025-09-18 18:55:55 -0400

    more code

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  output: "export",          // 🔑 Genera HTML estático
+  images: { unoptimized: true }, // Evita problemas con <Image />
+  basePath: "/NOMBRE_REPO",  // 👈 cambia por el nombre de tu repo en GitHub
+  assetPrefix: "/NOMBRE_REPO/",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
