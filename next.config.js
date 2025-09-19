c3628c0bffd54357d45b37fc534a70e7c46eacd9
Arcanik <loisgordisrour@gmail.com>
2025-09-18 22:09:10 -0400

    configurando para netly

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",          // 🔑 Genera HTML estático
-  images: { unoptimized: true }, // Evita problemas con <Image />
-  basePath: "/metodos-simulacion",  // 👈 cambia por el nombre de tu repo en GitHub
-  assetPrefix: "/metodos-simulacion/",
+  images: { unoptimized: true }, 
 };
 
 module.exports = nextConfig;
